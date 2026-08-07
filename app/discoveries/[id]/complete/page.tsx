3f6016c3e0e056719d80cb44e77d286887641a4e
assa-rabiya <a99283288@gmail.com>
2026-08-07 09:20:39 +0900

    fix: remove dynamic completion route

--- app/discoveries/[id]/complete/page.tsx
+++
@@ -1,9 +0,0 @@
-import DiscoveryCompleteClient from "./client";
-
-export function generateStaticParams() {
-  return [{ id: "view" }];
-}
-
-export default function DiscoveryCompletePage() {
-  return <DiscoveryCompleteClient />;
-}
